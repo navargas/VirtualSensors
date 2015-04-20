5f877bc442b37dd053fdd1d9a119be343cd00020
Nick Vargas <nickvargas95@gmail.com>
2015-04-20 11:13:50 -0700

    User creation and login

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -3,24 +3,51 @@ var express = require('express');
 
 var router = express.Router();
 
-//  if (!sessions.verify(req.body.token)) res.end('noauth');
-//var route = function(
 router.get('/', function(req, res) {
   res.end('POST {username:..., password:..} to /v1/auth/session');
 });
 
-router.post('/sessions', function(req, res) {
-  if (req.body.username && req.body.password) {
-    var token = sessions.authenticate(req.body.username, req.body.password);
-    res.send({"token":token});
-  } else {
-    res.send({"error":"Username or password not provided. Post JSON data"});
-  }
+router.get('/log', function(req, res) {
+  sessions.log();
   res.end();
 });
 
-router.post('/accounts', function(req, res) {
-  res.send(sessions.data);
+router.post('/login', function(req, res) {
+  if (req.headers['content-type'].indexOf('json') < 0) {
+    res.end('Content type must be application/json');
+    return;
+  }
+  if (req.body.username && req.body.password) {
+    sessions.authenticate(req.body.username, req.body.password, function(err, token) {
+      if (err) {
+        res.send({"error":err});
+        res.end();
+      } else {
+        res.send({"token":token});
+        res.end();
+      }
+    });
+  } else {
+    res.send({"error":"Username or password not provided. Post JSON data"});
+    res.end();
+  }
+});
+
+router.post('/users', function(req, res) {
+  if (req.body.username && req.body.password) {
+    sessions.createUser(req.body, function(err, data) {
+      if (err) {
+        res.send({"error":err});
+        res.end();
+      } else {
+        res.send({"username":req.body.username, "status":"success"});
+        res.end();
+      }
+    });
+  } else {
+    res.send({"error":"Username/password not provided"});
+    res.end();
+  }
 });
 
 module.exports = router;
