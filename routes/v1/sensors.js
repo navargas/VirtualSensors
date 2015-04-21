7372139009d96822fc0414f565fe9c888634a0a3
Nick Vargas <nickvargas95@gmail.com>
2015-04-21 12:22:06 -0700

    Add sample device

--- routes/v1/sensors.js
+++ routes/v1/sensors.js
@@ -3,7 +3,12 @@ var sessions = require('../../lib/sessions.js');
 var router = express.Router();
 
 router.get('/', sessions.verify, function(req, res) {
-   res.end('ok sensors');
+  res.send([ {
+    "name":"exampleSensor1",
+    "id":"uid1237324982374983274",
+    "value":3
+  }]);
+  res.end();
 });
 
 module.exports = router;
