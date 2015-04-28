6e750c386d0ef8b44d70e843c3da6ecfea2a2dcf
Nick Vargas <nickvargas95@gmail.com>
2015-04-28 00:53:25 -0700

    Add sensor reporting functionality

--- routes/v1/sensors.js
+++ routes/v1/sensors.js
@@ -3,11 +3,23 @@ var sessions = require('../../lib/sessions.js');
 var router = express.Router();
 
 router.get('/', sessions.verify, function(req, res) {
-  res.send([ {
-    "name":"exampleSensor1",
-    "id":"uid1237324982374983274",
-    "value":3
-  }]);
+  var devs = Object.keys(sessions.getDevices(req));
+  console.log("Devices", devs);
+  res.send(devs);
+  res.end();
+});
+
+router.post('/', sessions.verify, function(req, res) {
+  console.log("Adding device:", req.body);
+  var info = sessions.addDevice(req, req.body.device);
+  res.send(info);
+  res.end();
+});
+
+router.delete('/:sensor', sessions.verify, function(req, res) {
+  //get sensor info
+  var info = sessions.removeDevice(req, uid);
+  res.send(info);
   res.end();
 });
 
